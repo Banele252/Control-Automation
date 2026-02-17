ac5b401c9a49e628dfb2c80453ead275add0c4d6
Banele Mdluli <banele.mdluli25@gmail.com>
2026-02-17 23:32:13 +0200

    Added an in dev API

---
+++ BackEnd/Supabase/canvas_tests.py
@@ -0,0 +1,40 @@
+
+# Import the required libraries 
+
+from fastapi import FastAPI
+from supabase import create_client, Client
+import os
+from dotenv import load_dotenv
+
+# Provide a brief description of the task
+
+"""
+
+
+This is a wrapper Fast API to call supabase API and retrieve the stored in that database.
+
+
+"""
+load_dotenv()
+#Load the necessary keys
+
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+
+print(SUPABASE_KEY)
+
+# Create a client with supabase
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+
+
+app = FastAPI()
+
+@app.get("/data")
+async def get_data():
+    try:
+        response = supabase.table('test_data').select("*").execute()
+    except:
+        response = {'details':"Error"}
+    return response
