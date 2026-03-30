22fad89bb598b2c7e631d47f576dbc5edf82f38d
Banele Mdluli <banele.mdluli25@gmail.com>
2026-03-30 13:04:51 +0200

    changed supabase file

--- BackEnd/Supabase/supabase_create_tables.py
+++ BackEnd/Supabase/supabase_create_tables.py
@@ -1,6 +1,6 @@
 # create_tables.py
-from canvas_databases import Base, engine
-import canvas_models   # ensures all models are registered with Base.metadata
+from supabase_databases import Base, engine
+import supabase_models   # ensures all models are registered with Base.metadata
 
 ## -TODO 
 # We need to create a workflow that executes this python file.
