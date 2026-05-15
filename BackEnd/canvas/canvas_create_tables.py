9f9ba9c89c68a2166bf51134175b446bea9fdc05
ovatee <tshepo.dikgale@outlook.com>
2026-05-15 16:35:08 +0200

    message

--- BackEnd/canvas/canvas_create_tables.py
+++ BackEnd/canvas/canvas_create_tables.py
@@ -2,6 +2,8 @@
 from canvas_databases import Base, engine
 import canvas_models   # ensures all models are registered with Base.metadata
 
+#something
+
 ## -TODO 
 # We need to create a workflow that executes this python file.
 print("Creating tables...")
