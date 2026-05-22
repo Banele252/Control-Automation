f5ed7773928a03f547b9e966b62fea1612127bd0
banele252 <banele.mdluli25@gmail.com>
2026-05-22 22:58:31 +0200

    Checking the kubernetes file

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -10,6 +10,7 @@ app = FastAPI()
 async def healthy_check():
     return {"message": "Healthy"}
 
+
 app.include_router(interactive_main.router)
 app.include_router(summary_main.router)
 app.include_router(supabase_main.router)
