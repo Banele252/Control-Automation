0dfdc57122a1d833e254e1a2c11cedeba07bb31b
banele252 <banele.mdluli25@gmail.com>
2026-06-20 18:59:56 +0200

    gradio app that interracts with the agent has been created for dev use

---
+++ FrontEnd/gradio/gradio_interface.py
@@ -0,0 +1,33 @@
+import gradio as gr
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+port = os.getenv("BACKEND_PORT", "8000")
+backend_container = os.getenv("BACKEND_CONTAINER", "localhost")
+
+
+def chat(message, history):
+    messages = message
+    #BaseURL = f"http://{backend_container}:{port}" # use this in production
+    BaseURL = f"http://localhost:8000" # to be used in local development
+    endpoint = "/interactive-agent/interactive-agent"
+    url = BaseURL + endpoint
+    params = {
+        "message": messages
+    }
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        data = response.json()
+        return data.get('result', 'No result found')
+    except requests.exceptions.RequestException as e:
+        print(f"Error occurred: {e}")
+        return "Error occurred while fetching response"
+    
+
+demo = gr.ChatInterface(
+    fn=chat
+    )
+demo.launch()
